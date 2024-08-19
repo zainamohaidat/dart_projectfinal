3e1be95727fe7cd8ae4497a905599a2428f9c5ad
zainamohaidat <zainamohidat@gmail.com>
2024-08-19 04:08:12 +0300

    Delete final01/library management1/dart_application_1 directory

--- final01/library management1/dart_application_1/bin/listcustomer.dart
+++
@@ -1,8 +0,0 @@
-import 'customers.dart';
-
-List<Customers> customerList = [
-  Customers("Ahmad", "A@gmaile.com", "2020"),
-  Customers("Saleem", "S@gmaile.cpm", "2021"),
-  Customers("Remas", "R@gmaile.com", "2008"),
-  Customers("Abdallah", "Ab@gamile.com", "2024")
-];
